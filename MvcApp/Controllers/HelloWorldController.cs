da9de16bcb29dee0bd068d7be06122558fdef658
mvs79 <friesentee79@gmail.com>
2017-02-25 15:29:17 +0100

    masterbranch1

---
+++ MvcApp/Controllers/HelloWorldController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApp.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+
+
+        // 
+        // GET: /HelloWorld/ 
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // 
+        // GET: /HelloWorld/Welcome/ 
+        public ActionResult Welcome(string name,int numTimes = 1)
+        {
+            ViewBag.Message = name;
+            ViewBag.NumTimes = numTimes;
+            return View();
+        }
+       
+      
+        
+
+    }
+}
